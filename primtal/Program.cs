334b85d497ade0f86208ff20b9ac698d65116262
viktorx64 <viktor.sundqvist2@gmail.com>
2021-01-13 11:30:27 +0100

    made the test if it is a prime number method

--- primtal/Program.cs
+++ primtal/Program.cs
@@ -10,6 +10,23 @@ namespace primtal
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(primeNumer(6));
+            Console.WriteLine(primeNumer(5));
+        }
+
+        private static int primeNumer(int num) {
+            //make it test if it is a prime number or not
+            int i;
+            for (i = 2; i < num; i++) {
+                //test if it is dividable or not
+                if (num % i == 0) {
+                    return 0;
+                }
+            }
+            if (i == num) {
+                return 1;
+            }
+            return 0;
         }
     }
 }
